a6e75ce5019c41248d62178cf66be6b950d13447
9names <60134748+9names@users.noreply.github.com>
2025-12-23 11:18:23 +1100

    Update with wlink runner now that my wchlink-e is updated

--- ch32v006_blinky/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-riscv64-unknown-elf-objcopy $1  -O binary out.bin
-minichlink -w ./out.bin flash -b 
-
--- ch32v006_blinky/src/main.rs
+++ ch32v006_blinky/src/main.rs
@@ -8,19 +8,25 @@
 
 use hal::delay::Delay;
 use hal::gpio::{Level, Output};
+use hal::println;
 use {ch32_hal as hal, panic_halt as _};
 
 #[qingke_rt::entry]
 fn main() -> ! {
-    // hal::debug::SDIPrint::enable();
-    let mut config = hal::Config::default();
-    config.rcc = hal::rcc::Config::SYSCLK_FREQ_24MHZ_HSE;
-    let p = hal::init(config);
+    hal::debug::SDIPrint::enable();
+    // Print something immediately to confirm that SDI is working
+    println!("hello world!");
+
+    let p = hal::init(ch32_hal::Config {
+        rcc: hal::rcc::Config::SYSCLK_FREQ_24MHZ_HSE,
+        ..Default::default()
+    });
 
     let mut delay = Delay;
 
     let mut led = Output::new(p.PD0, Level::Low, Default::default());
     let mut led2 = Output::new(p.PC0, Level::Low, Default::default());
+
     loop {
         led.set_high();
         delay.delay_ms(1000);
