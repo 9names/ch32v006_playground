05b273ab8a30bf2984cc7a32aa0d5ab4b3db72b3
9names <60134748+9names@users.noreply.github.com>
2025-12-23 23:22:08 +1100

    Add clock config for HSI

--- ch32v006_blinky/src/main.rs
+++ ch32v006_blinky/src/main.rs
@@ -6,10 +6,11 @@
 // LEDs on ch32v006 evt are not connected to anything by default.
 // I've jumpered them to D0 and C0
 
+use ch32_hal as hal;
 use hal::delay::Delay;
 use hal::gpio::{Level, Output};
 use hal::println;
-use {ch32_hal as hal, panic_halt as _};
+use panic_halt as _;
 
 #[qingke_rt::entry]
 fn main() -> ! {
@@ -17,17 +18,31 @@ fn main() -> ! {
     // Print something immediately to confirm that SDI is working
     println!("hello world!");
 
-    let p = hal::init(ch32_hal::Config {
-        rcc: hal::rcc::Config::SYSCLK_FREQ_24MHZ_HSE,
+    // For HSE
+    // let p = hal::init(ch32_hal::Config {
+    //     rcc: hal::rcc::Config::SYSCLK_FREQ_24MHZ_HSE,
+    //     ..Default::default()
+    // });
+
+    // For HSI we need to mess with AHB divider (48Mhz is too much? need to check datasheet)
+    use ch32_hal::pac::rcc::vals::Hpre;
+    let mut config = ch32_hal::Config {
+        rcc: hal::rcc::Config::SYSCLK_FREQ_48MHZ_HSI,
         ..Default::default()
-    });
+    };
+    config.rcc.ahb_pre = Hpre::DIV2;
+    let p = hal::init(config);
+
+    println!("still alive!");
 
     let mut delay = Delay;
 
     let mut led = Output::new(p.PD0, Level::Low, Default::default());
     let mut led2 = Output::new(p.PC0, Level::Low, Default::default());
-
+    let mut counter = 0;
     loop {
+        println!("loop {counter}");
+        counter += 1;
         led.set_high();
         delay.delay_ms(1000);
         led2.set_high();
